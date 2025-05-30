5620eda93069f709af40149e5399480fa8a1e7f3
CarloLaguda <carlo.laguda07@gmail.com>
2025-05-30 11:59:58 +0000

    Query Fatte

--- Project/routes_riders.py
+++ Project/routes_riders.py
@@ -25,5 +25,49 @@ def home():
         result.append(row_dict)
     return jsonify(result)
 
+@app.route("/MostPole/<int:numero>")
+def mostPole(numero):
+    mycursor.execute(f"SELECT Driver,Pole_Positions FROM RIDERS.motogp ORDER BY Pole_Positions DESC LIMIT {numero};")
+    myresult = mycursor.fetchall()
+    column_names = [desc[0] for desc in mycursor.description]
+    result = []
+    for row in myresult:
+        row_dict = dict(zip(column_names, row))
+        result.append(row_dict)
+    return jsonify(result)
+
+@app.route("/MostWin/<int:numero>")
+def mostWin(numero):
+    mycursor.execute(f"SELECT Driver,Victories FROM RIDERS.motogp ORDER BY Victories DESC LIMIT {numero};")
+    myresult = mycursor.fetchall()
+    column_names = [desc[0] for desc in mycursor.description]
+    result = []
+    for row in myresult:
+        row_dict = dict(zip(column_names, row))
+        result.append(row_dict)
+    return jsonify(result)
+
+@app.route("/MostWin_Championship/<int:numero>")
+def mostWin_Champio(numero):
+    mycursor.execute(f"SELECT Driver,World_Championships FROM RIDERS.motogp ORDER BY World_Championships DESC LIMIT {numero};")
+    myresult = mycursor.fetchall()
+    column_names = [desc[0] for desc in mycursor.description]
+    result = []
+    for row in myresult:
+        row_dict = dict(zip(column_names, row))
+        result.append(row_dict)
+    return jsonify(result)
+
+@app.route("/Nations/<nazione>")
+def nation(nazione):
+    mycursor.execute("SELECT * FROM RIDERS.motogp WHERE Nationality = %s;", (nazione,))
+    myresult = mycursor.fetchall()
+    column_names = [desc[0] for desc in mycursor.description]
+    result = []
+    for row in myresult:
+        row_dict = dict(zip(column_names, row))
+        result.append(row_dict)
+    return jsonify(result)
+
 if __name__ == "__main__":
     app.run(debug = True)
